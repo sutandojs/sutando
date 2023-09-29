25822f20182273ed162283c8cd30c357eb0dd930
kiddyu <58631254@qq.com>
2023-09-30 00:23:36 +0800

    support global scopes

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,5 @@
+const isArray = require('lodash/isArray');
+
 class BaseError extends Error {
   constructor(message, entity) {
     super(message);
@@ -8,10 +10,38 @@ class BaseError extends Error {
   }
 }
 
-class ModelNotFoundError extends BaseError {}
+class ModelNotFoundError extends BaseError {
+  model;
+  ids;
+
+  setModel(model, ids = []) {
+    this.model = model;
+    this.ids = isArray(ids) ? ids : [ids];
+
+    this.message = `No query results for model [${model}]`;
+
+    if (this.ids.length > 0) {
+      this.message += ' ' + this.ids.join(', ');
+    } else {
+      this.message += '.';
+    }
+
+    return this;
+  }
+
+  getModel() {
+    return this.model;
+  }
+
+  getIds() {
+    return this.ids;
+  }
+}
 class RelationNotFoundError extends BaseError {}
+class InvalidArgumentError extends BaseError {}
 
 module.exports = {
   ModelNotFoundError,
   RelationNotFoundError,
+  InvalidArgumentError,
 };
