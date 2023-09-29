769591c74dc75afd22d4d64ef72dcf753f83cda0
kiddyu <58631254@qq.com>
2023-09-30 01:44:17 +0800

    add `setHidden` `setVisible` methods

--- src/concerns/hides-attributes.js
+++ src/concerns/hides-attributes.js
@@ -8,7 +8,7 @@ const HidesAttributes = (Model) => {
     makeVisible(...keys) {
       const visible = _.flatMapDeep(keys);
       this.visible = [...this.visible, ...visible];
-  
+
       this.hidden = _.difference(this.hidden, visible);
       return this;
     }
@@ -16,11 +16,27 @@ const HidesAttributes = (Model) => {
     makeHidden(...keys) {
       const hidden = _.flatMapDeep(keys);
       this.hidden = [...this.hidden, ...hidden];
-  
+
       this.visible = _.difference(this.visible, hidden);
       return this;
     }
+
+    getHidden() {
+      return this.hidden;
+    }
+
+    getVisible() {
+      return this.visible;
+    }
+
+    setHidden(hidden) {
+      this.hidden = hidden;
+    }
+
+    setVisible(visible) {
+      this.visible = visible;
+    }
   }
-};
+}
 
 module.exports = HidesAttributes;
